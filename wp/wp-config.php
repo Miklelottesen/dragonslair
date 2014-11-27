b2088b9f6497448a338eaeaf2f3d2d7ca61f8b54
Miklelottesen <miklelottesen@gmail.com>
2014-11-27 14:45:19 +0100

    Moved database online!!!! :D

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -16,16 +16,18 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp_dragonslair');
+
+
+define('DB_NAME', 'dragonslair');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'teamcandy');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '123456');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'db4free.net');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
