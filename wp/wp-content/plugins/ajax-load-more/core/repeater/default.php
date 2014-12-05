a848472961d0061b51a5f682f06ccebf725f5ca4
Miklelottesen <miklelottesen@gmail.com>
2014-12-05 21:55:00 +0100

    Changed AJAX repeater

--- wp/wp-content/plugins/ajax-load-more/core/repeater/default.php
+++ wp/wp-content/plugins/ajax-load-more/core/repeater/default.php
@@ -1,4 +1,5 @@
 <li>
+<article>
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(array(100,100));
 }?>
 <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
@@ -6,4 +7,5 @@
     <?php the_time("F d, Y"); ?>
 </p>
 <?php the_excerpt(); ?>
+</article>
 </li>
