60eb2b55f0bac8711f9fb8d2d309424024d5bf33
Miklelottesen <miklelottesen@gmail.com>
2014-12-11 11:35:42 +0100

    Enabled post thumbs

--- wp/wp-content/themes/dragonslair/functions.php
+++ wp/wp-content/themes/dragonslair/functions.php
@@ -4,4 +4,6 @@
 		register_nav_menu('header-menu',__( 'Header Menu' ));
 	}
 	add_action( 'init', 'register_my_menu' );
+	// Neccessary to enable post thumbnails:
+	add_theme_support( 'post-thumbnails' ); 
 ?>
